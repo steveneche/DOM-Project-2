c41b4366edbb02814484b9628dcbd26ff6ec4505
steveneche <steveneche@gmail.com>
2025-07-30 16:18:49 +0100

    adding comments

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// selecting all html element
 const btn = document.getElementById("change-color-btn")
 const colorBox = document.getElementById("color-box")
+// adding event listener
 btn.addEventListener("click", function(){
     colorBox.style.backgroundColor= getRandomColor()
 })
+
+// creating a function generate random number
 function getRandomColor(){
     return "rgb(" + randNumber() +"," +randNumber() + ","+randNumber() +")"  
 }
